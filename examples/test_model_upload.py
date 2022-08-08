3bd781ab040b0240a50953acdf9e8d196e631067
Sherry <sherry.shuang.yu@gmail.com>
2022-08-09 00:58:59 +1000

    updated the model profile example

--- examples/test_model_upload.py
+++ examples/test_model_upload.py
@@ -6,8 +6,8 @@ import time
 
 service_url = "http://wmla-mgmt1.sls30lab.com:9000"
 service_instance = "ANZ-DLI-IG"
-username = "UNAME"
-password = "PASSWORD"
+username = "jbtang"
+password = "demoexec"
 
 edi_connection = Connection(service_url, service_instance, wmla_v1=True, edi=True,
                  apikey=None, username=username, password=password)
@@ -31,7 +31,7 @@ def delete_model(model_name):
 
 def start_model(model_name):
     file_handle = open("mnist_example/mnist.tar", "rb")
-    result = conn.deploy_model(userfile=file_handle, timeout = 300)
+    result = conn.deploy_model(userfile = file_handle, timeout = 300)
 
 
     response = conn.get_model_profile(model_name)
@@ -41,23 +41,7 @@ def start_model(model_name):
                     'shared', 'GPUHosts',
                     '/ANZ/ANZ-DLI-IG/ANZ-DLI-IG-sparkexecutor/ANZ-DLI-IG-sparkexecutor1')
 
-    # print('Updated model profile: ', model_profile)
-
-
-    kernel = model_profile['kernel']
-    name = model_profile['name']
-    type = model_profile['type']
-    policy = model_profile['policy']
-    replica = model_profile['replica']
-    resource_allocation = model_profile['resource_allocation']
-    schema_version = model_profile['schema_version']
-    create_time = model_profile['create_time']
-    last_update_time = model_profile['last_update_time']
-
-    response = conn.update_model_profile(model_name, kernel = kernel, name = name, policy = policy, 
-                                            replica = replica, resource_allocation = resource_allocation,
-                                            schema_version = schema_version, type = type, 
-                                            create_time = create_time, last_update_time = last_update_time)
+    response = conn.update_model_profile(model_name, model_profile)
 
     response = conn.get_model_profile(model_name)
 
@@ -87,10 +71,18 @@ def infer(model_name):
 # delete_model(model_name)
 # start_model(model_name)
 
-response = conn.get_model(model_name)
-print(response.result)
+# response = conn.get_model(model_name)
+# print(response.result)
 
-response = conn.get_model_instance(model_name)
-print(response.result)
+# response = conn.get_model_instance(model_name)
+# print(response.result)
 
-infer(model_name)
+response = conn.get_model_profile(model_name)
+model_profile = response.result
+
+update_model_profile_parameters(model_profile,
+                'shared', 'GPUHosts',
+                '/ANZ/ANZ-DLI-IG/ANZ-DLI-IG-sparkexecutor/ANZ-DLI-IG-sparkexecutor1')
+response = conn.update_model_profile(model_name, model_profile)
+print(response)
+# infer(model_name)
