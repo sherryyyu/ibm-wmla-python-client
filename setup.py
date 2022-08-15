97ae0b35a1afd10df18992fb489c9c33061cd02a
Sherry <sherry.shuang.yu@gmail.com>
2022-08-15 18:36:34 +1000

    modified mnist notebook, added Adam in pyproject.toml, updated version in setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ import os
 import sys
 import pkg_resources
 
-__version__ = '0.0.1'
+__version__ = '0.1.3'
 PACKAGE_NAME = 'ibm_wmla_client'
 PACKAGE_DESC = 'Python client library for IBM Watson Machine Learning Accelerater'
 
